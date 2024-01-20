f9b0adf230431153b723bc6e433569fff9b3c0ca
StevenKight <sgk0711@gmail.com>
2024-01-20 16:19:02 -0500

    Implement communication between desktop and ASP.Net.

--- CapstoneGroup2/Desktop Client/MainPage.xaml.cs
+++ CapstoneGroup2/Desktop Client/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Desktop_Client.Dal;
+using Desktop_Client.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -9,11 +12,29 @@ namespace Desktop_Client
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Data members
+
+        private List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+
+        #endregion
+
         #region Constructors
 
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.loadWeatherForecasts();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void loadWeatherForecasts()
+        {
+            this.weatherForecasts = await WeatherDal.GetWeatherForecastsAsync();
+            this.ForecastListView.ItemsSource = this.weatherForecasts;
         }
 
         #endregion
