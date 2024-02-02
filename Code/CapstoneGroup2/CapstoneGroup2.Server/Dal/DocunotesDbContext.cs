dfe9a7b5af5c9b6f3ae76327017fa6ac9c883191
StevenKight <sgk0711@gmail.com>
2024-02-01 21:28:13 -0500

    Create powershell script for testing and generating html coverage reports.

---
+++ Code/CapstoneGroup2/CapstoneGroup2.Server/Dal/DocunotesDbContext.cs
@@ -0,0 +1,42 @@
+﻿using API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Dal;
+
+public class DocunotesDbContext : DbContext
+{
+    #region Properties
+
+    public User? CurrentUser { get; set; }
+
+    public DbSet<Note> Notes { get; set; }
+
+    public DbSet<Shared> SharedNotes { get; set; }
+
+    public DbSet<Source> Sources { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public DocunotesDbContext(DbContextOptions<DocunotesDbContext> options) : base(options)
+    {
+    }
+
+    #endregion
+
+    #region Methods
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .ApplyConfiguration(new NoteConfiguration())
+            .ApplyConfiguration(new SharedConfiguration())
+            .ApplyConfiguration(new SourceConfiguration())
+            .ApplyConfiguration(new UserConfiguration());
+    }
+
+    #endregion
+}
