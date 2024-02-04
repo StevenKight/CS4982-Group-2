c80a0ca2e0069954e014225a7496705cfb514759
TurtleHermit12 <ahanson@121g.io>
2024-02-04 01:09:14 -0500

    Add DAL for source,User,and update Notes DAL
    Add functionality for server communication to all DAL

--- Code/CapstoneGroup2/Desktop Client/Dal/NotesDal.cs
+++ Code/CapstoneGroup2/Desktop Client/Dal/NotesDal.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using Desktop_Client.Model;
+using Windows.Web.Http;
+using HttpClient = System.Net.Http.HttpClient;
+using HttpResponseMessage = System.Net.Http.HttpResponseMessage;
 
 namespace Desktop_Client.Dal
 {
@@ -11,20 +15,30 @@ namespace Desktop_Client.Dal
     {
         #region Data members
 
-        private static readonly HttpClient client = new HttpClient();
+        private System.Net.Http.HttpClient client;
 
         private static readonly string baseUrl = "https://localhost:7041/";
 
         #endregion
 
+        public NotesDal()
+        {
+            this.client = new HttpClient();
+        }
+
+        public NotesDal(HttpClient client)
+        {
+            this.client = client;
+        }
+
         #region Methods
 
-        public static async Task<List<UserNote>> GetUsersNotesAsync()
+        public async Task<List<UserNote>> GetUsersNotesAsync()
         {
-            client.BaseAddress = new Uri(baseUrl);
+            this.client.BaseAddress = new Uri(baseUrl);
 
             var notes = new List<UserNote>();
-            var response = await client.GetAsync("Notes");
+            var response = await this.client.GetAsync("Notes");
             if (response.IsSuccessStatusCode)
             {
                 notes = await response.Content.ReadFromJsonAsync<List<UserNote>>();
@@ -33,6 +47,62 @@ namespace Desktop_Client.Dal
             return notes;
         }
 
+        public async  Task<bool> createNewNote(Note note, string authToken)
+        {
+            client.BaseAddress = new Uri(baseUrl);
+
+            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(note);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
+
+            HttpResponseMessage response = await this.client.PostAsync("Notes", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> UpdateNote(Note note, string authToken)
+        {
+            client.BaseAddress = new Uri(baseUrl);
+
+            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(note);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            this.client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
+
+            HttpResponseMessage response = await this.client.PutAsync("Notes", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> DeleteNote(Note note, string authToken)
+        {
+            this.client.BaseAddress = new Uri(baseUrl);
+
+            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(note);
+
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            this.client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
+
+            HttpResponseMessage response = await this.client.DeleteAsync("Notes");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
         #endregion
     }
 }
---
+++ Code/CapstoneGroup2/Desktop Client/Dal/SourceDal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using System.Net.Http;
+using Desktop_Client.Model;
+using System.IO;
+using Windows.Storage.Streams;
+
+namespace Desktop_Client.Dal
+{
+    public class SourceDal
+    {
+        private HttpClient client;
+
+        private static readonly string baseUrl = "https://localhost:7041/";
+
+        public SourceDal()
+        {
+            this.client = new HttpClient();
+        }
+
+        public SourceDal(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<List<Source>> getSourcesForUser(User user)
+        {
+            var sources = new List<Source>();
+            var response = await client.GetAsync(new Uri($"Source/{user.Username}"));
+            if (response.IsSuccessStatusCode)
+            {
+                sources = await HttpContentJsonExtensions.ReadFromJsonAsync<List<Source>>(response.Content);
+                return sources;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<List<Source>> getSources()
+        {
+            var sources = new List<Source>();
+            var response = await client.GetAsync(new Uri($"Source"));
+            if (response.IsSuccessStatusCode)
+            {
+                sources = await HttpContentJsonExtensions.ReadFromJsonAsync<List<Source>>(response.Content);
+                return sources;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task SendPdfToServer(User user, byte[] pdfBytes)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                        ByteArrayContent byteArrayContent = new ByteArrayContent(pdfBytes);
+
+                        // Send the POST request
+                        HttpResponseMessage response = await client.PostAsync($"Source/{user.Username}", byteArrayContent);
+
+                        // Check if the request was successful
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("PDF file uploaded successfully.");
+                        }
+                        else
+                        {
+                            // Handle the error (log, display, etc.)
+                            Console.WriteLine($"Error uploading PDF file: {response.StatusCode}");
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (log, display, etc.)
+                Console.WriteLine($"Error uploading PDF file: {ex.Message}");
+            }
+        }
+
+        static async Task DownloadFileAsync(int sourceId, string filePath)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync($"Source/{sourceId}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (Stream responseStream = await response.Content.ReadAsStreamAsync())
+                        {
+                            using (FileStream fileStream = File.Create(filePath))
+                            {
+                                await responseStream.CopyToAsync(fileStream);
+                            }
+                        }
+
+                        Console.WriteLine($"File downloaded successfully: {filePath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error downloading file: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error downloading file: {ex.Message}");
+            }
+        }
+
+    }
+}
