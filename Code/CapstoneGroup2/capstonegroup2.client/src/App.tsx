7f7b27205bd99c734afa46ec1a3577c3a7dddc5f
StevenKight <sgk0711@gmail.com>
2024-02-07 03:30:23 -0500

    Begin rework of web client UI.

--- Code/CapstoneGroup2/capstonegroup2.client/src/App.tsx
+++ Code/CapstoneGroup2/capstonegroup2.client/src/App.tsx
@@ -1,50 +1,40 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import PostAuthorize from './pages/Dashboard.tsx';
-import Dashboard from './pages/Dashboard';
-import NavigationBar from './components/NavigationBar';
-import MySources from './pages/MySources.tsx';
-import SharedSources from './pages/SharedSources.tsx';
+
+import Authorize from './routes/Authorize';
+import Postauthorize from './routes/Postauthorize';
+import { useEffect } from 'react';
 
 function App() {
-    const [auth, setAuth] = useState<boolean>(false);
 
-    const handleLogin = () => {
-        setAuth(localStorage.getItem('token') !== null);
-    };
+    const [auth, setAuth] = useState<boolean>();
 
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        setAuth(false);
-    };
+    useEffect(() => {
+        checkAuth();
 
-    const handleRegister = () => {
-        setAuth(false);
-    };
+        window.addEventListener('storage', checkAuth)
+
+        return () => {
+            window.removeEventListener('storage', checkAuth)
+        }
+    }, []);
+
+    const checkAuth = () => {
+        console.log('checking auth');
+        const auth = Boolean(localStorage.getItem('auth') ?? false);
+        setAuth(auth);
+    }
 
     return (
-        <div>
-            <BrowserRouter>
-                {auth && <NavigationBar onLogout={handleLogout} />} {}
-                <Routes>
-                    {
-                        !auth ?
-                            <>
-                                <Route path="/" index element={<Login onLogin={handleLogin} />} />
-                                <Route path="/register" element={<Register onRegister={handleRegister} />} />
-                            </> :
-                            <>
-                                <Route path="/" index element={auth ? <PostAuthorize /> : null} />
-                                <Route path="/dashboard" element={auth ? <Dashboard /> : null} />
-                                <Route path='/mysources' element={<MySources />} />
-                                <Route path='/sharedsources' element={<SharedSources />} />
-                            </>
-                    }
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                {
+                    !auth ?
+                        <Route path='*' element={<Authorize />} /> :
+                        <Route path='*' element={<Postauthorize />} />
+                }
+            </Routes>
+        </BrowserRouter>
     );
 }
 
