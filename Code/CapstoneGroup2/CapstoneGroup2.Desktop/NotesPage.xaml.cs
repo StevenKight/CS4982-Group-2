3fdfa34469c221e657d2bc1bea6f341698527562
StevenKight <sgk0711@gmail.com>
2024-02-19 23:19:54 -0500

    Update note text display in desktop client.
    Allows wrapping and more advanced selection display to make it easier to look at the whole note at once.

--- Code/CapstoneGroup2/CapstoneGroup2.Desktop/NotesPage.xaml.cs
+++ Code/CapstoneGroup2/CapstoneGroup2.Desktop/NotesPage.xaml.cs
@@ -242,7 +242,18 @@ namespace CapstoneGroup2.Desktop
             var selectedItemTextBox = (Note)textBox.DataContext;
             selectedItemTextBox.NoteText = textBox.Text;
 
+            if (string.IsNullOrEmpty(selectedItemTextBox.NoteText))
+            {
+                selectedItemTextBox.NoteText = "Enter Note...";
+            }
+
             await this._notesViewModel.updateNote(selectedItemTextBox);
+
+            textBox.MaxHeight = 30;
+
+            var stackPanel = (Grid)textBox.Parent;
+            var textBlock = (TextBlock)stackPanel.Children[1];
+            textBlock.Visibility = Visibility.Visible;
         }
 
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
@@ -251,6 +262,12 @@ namespace CapstoneGroup2.Desktop
             var selectedItemTextBox = (Note)textBox.DataContext;
 
             this.notesListView.SelectedItem = selectedItemTextBox;
+
+            textBox.MaxHeight = 100;
+
+            var stackPanel = (Grid)textBox.Parent;
+            var textBlock = (TextBlock)stackPanel.Children[1];
+            textBlock.Visibility = Visibility.Collapsed;
         }
 
         #endregion
