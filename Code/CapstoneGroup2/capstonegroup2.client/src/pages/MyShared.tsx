bec0f0ecdda2b5a1f7fc3d58de965779e819c781
Ahanson121g <128514087+Ahanson121g@users.noreply.github.com>
2024-03-01 11:57:49 -0500

    Add Support for hosted video to web client. Extract components from MySourceNotes for simplification. Add VideoPlayer component for displaying videos. Update MySourceNotesPage for displaying video and matching desktop
    Add PdfViewer component to support separation of concerns.
    Add VideoPlayer component for displayig videos in web client
    Update MySourceNotesPage to support displaying video.
    Update MySourceNotesPage and css to match the styling of the desktop application

--- Code/CapstoneGroup2/capstonegroup2.client/src/pages/MyShared.tsx
+++ Code/CapstoneGroup2/capstonegroup2.client/src/pages/MyShared.tsx
@@ -1,17 +1,17 @@
-import SourcesGrid from "../components/SourcesGrid";
+//import SourcesGrid from "../components/SourcesGrid";
 
-import { Source, SourceType } from "../interfaces/Source";
+//import { Source, SourceType } from "../interfaces/Source";
 
-const dummySources: Source[] = [
-    { sourceId: 1, name: "Source 1", description: "Description 1", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 2, name: "Source 2", description: "Description 2", isLink: false, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 3, name: "Source 3", description: "Description 3", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 4, name: "Source 4", description: "Description 4", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 5, name: "Source 5", description: "Description 5", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 6, name: "Source 6", description: "Description 6", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 7, name: "Source 7", description: "Description 7", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-    { sourceId: 8, name: "Source 8", description: "Description 8", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
-];
+//const dummySources: Source[] = [
+//    { sourceId: 1, name: "Source 1", description: "Description 1", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 2, name: "Source 2", description: "Description 2", isLink: false, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 3, name: "Source 3", description: "Description 3", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 4, name: "Source 4", description: "Description 4", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 5, name: "Source 5", description: "Description 5", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 6, name: "Source 6", description: "Description 6", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 7, name: "Source 7", description: "Description 7", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//    { sourceId: 8, name: "Source 8", description: "Description 8", isLink: true, link: "http://www.example.com", username: "StevenC", noteType: SourceType.Pdf, createdAt: new Date(), updatedAt: new Date()},
+//];
 
 /**
  * MyShared component displaying sources shared with the current user.
@@ -22,7 +22,7 @@ export default function MyShared() {
     return (
         <div>
             <h1>Dummy Shared with me</h1>
-            <SourcesGrid sources={dummySources} showUser/>
+            {/*<SourcesGrid sources={dummySources} showUser/>*/}
         </div>
     );
 }
